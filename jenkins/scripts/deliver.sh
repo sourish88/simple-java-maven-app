f445c240e28252a5e7d5ac8cdaf3d03102c2b252
sbanerjee66 <sbanerjee66@dxc.com>
2021-01-22 15:35:44 +0000

    build docker

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,3 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-
-echo 'Build Docker image'
-docker build -t sourish88/${NAME}:1.0.${BUILD_NUMBER} .
